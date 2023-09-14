45b232e082936a13e5ce9c59864702b32649596c
dipsingh100 <angelicsmile701@gmail.com>
2023-09-14 22:20:55 +0530

    set null for stacktrace in production

--- middleware/errHandler.js
+++ middleware/errHandler.js
@@ -6,33 +6,33 @@ const errHandler = (err, req, res, next) => {
             res.json({
                 title: "Invalid Request",
                 message: err.message,
-                stackTrace: err.stack
+                stackTrace: process.env.ENV === "development" ? err.stack : null
             })
             break
         case 401: res.json({
             title: "Unauthorized",
             message: err.message,
-            stackTrace: err.stack
+            stackTrace: process.env.ENV === "development" ? err.stack : null
         })
             break
         case 404:
             res.json({
                 title: "Not Found",
                 message: err.message,
-                stackTrace: err.stack
+                stackTrace: process.env.ENV === "development" ? err.stack : null
             })
             break
         case 409:
             res.json({
                 title: "Conflict",
                 message: err.message,
-                stackTrace: err.stack
+                stackTrace: process.env.ENV === "development" ? err.stack : null
             })
             break
         default: res.json({
             title: "Server Error",
             message: err.message,
-            stackTrace: err.stack
+            stackTrace: process.env.ENV === "development" ? err.stack : null
         })
     }
 }
